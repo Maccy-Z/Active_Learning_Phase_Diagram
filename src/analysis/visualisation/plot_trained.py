2d39487cfa0e55674956c83e921ab7dac4345e27
bubbles <mz406@cam.ac.uk>
2024-03-04 22:52:27 +0000

    Update plotting code

--- src/analysis/visualisation/plot_trained.py
+++ src/analysis/visualisation/plot_trained.py
@@ -80,8 +80,8 @@ def plot_single(T, ax):
     ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%.0f'))
     ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.0f'))
 
-def main():
 
+def main():
     n_rows, n_cols = 2, 5
     plot_steps = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
