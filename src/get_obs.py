2435b91c61dd3135e5362f0a6b452a233e6ea249
Your Name <you@example.com>
2024-05-20 18:58:52 +0100

    Eval ours on non square

--- src/get_obs.py
+++ src/get_obs.py
@@ -1,7 +1,10 @@
 from src.utils import ObsHolder
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-obs_holder = ObsHolder.load("./saves/12")
+obs_holder = ObsHolder.load("./saves/3")
 
 obs_pos, obs_phse, obs_probs = obs_holder.get_og_obs()
 
@@ -11,4 +14,11 @@ print()
 print(obs_phse.tolist())
 print()
 print(obs_probs.tolist())
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(obs_pos[:, 0], obs_pos[:, 1], obs_pos[:, 2], c=obs_phse, cmap='viridis')
+plt.show()
+
+
 
