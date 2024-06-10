f1be3a315dc36061187301cf1570454595f0f951
Your Name <you@example.com>
2024-06-10 01:12:41 +0100

    Cleanup for final

--- src/get_obs.py
+++ src/get_obs.py
@@ -1,24 +1,31 @@
+""" Plot observations from a save file"""
 from src.utils import ObsHolder
-import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-obs_holder = ObsHolder.load("./saves/3")
+obs_holder = ObsHolder.load("./saves/5")
 
 obs_pos, obs_phse, obs_probs = obs_holder.get_og_obs()
 
 
+print("Observation Locations")
 print(obs_pos.tolist())
-print()
+print("Observation Phases")
 print(obs_phse.tolist())
-print()
+print("Observation Probabilities")
 print(obs_probs.tolist())
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(obs_pos[:, 0], obs_pos[:, 1], obs_pos[:, 2], c=obs_phse, cmap='viridis')
-plt.show()
+if obs_holder.cfg.N_dim == 3:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(obs_pos[:, 0], obs_pos[:, 1], obs_pos[:, 2], c=obs_phse, cmap='viridis')
+    plt.show()
+else:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(obs_pos[:, 0], obs_pos[:, 1], c=obs_phse, cmap='viridis')
+    plt.show()
 
 
 
